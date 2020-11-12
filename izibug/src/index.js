7ad1ceaad3c5e5c813d02679fe75a0636f27340a
rafahmarinho <rafael.marinho96@hotmail.com>
2020-11-12 20:53:09 -0300

    up

--- izibug/src/index.js
+++ izibug/src/index.js
@@ -13,7 +13,7 @@ class LoginComponent extends React.Component {
     }
     toggleMode() {
         var newMode = this.state.mode === 'login' ? 'signup' : 'login';
-        this.setState({ mode: newMode});
+        this.setState({ mode: newMode });
     }
     render() {
         return (
@@ -25,6 +25,12 @@ class LoginComponent extends React.Component {
 
                         <div className="form-block__toggle-block">
                             <span>{this.state.mode === 'login' ? 'Não' : 'Já'} possui uma conta? Clique aqui &#8594;</span>
+                            <input id="form-toggler" type="checkbox" onClick={this.toggleMode.bind(this)} />
+                            <label htmlFor="form-toggler"></label>
+                        </div>
+
+                        <div className="form-block__toggle-block">
+                            <span> Esqueci minha senha &#8594;</span>
                             <input id="form-toggler" type="checkbox" onClick={this.toggleMode.bind(this)} />
                             <label htmlFor="form-toggler"></label>
                         </div>
@@ -44,28 +50,28 @@ class LoginForm extends React.Component {
     }
     render() {
         return (
-        <form onSubmit={this.props.onSubmit}>
-            <div className="form-block__input-wrapper">
-                <div className="form-group form-group--login">
-                    <Input type="text" id="username" label="E-mail" disabled={this.props.mode === 'signup'}/>
-                    <Input type="password" id="password" label="Senha" disabled={this.props.mode === 'signup'}/>
+            <form onSubmit={this.props.onSubmit}>
+                <div className="form-block__input-wrapper">
+                    <div className="form-group form-group--login">
+                        <Input type="text" id="username" label="E-mail" disabled={this.props.mode === 'signup'} />
+                        <Input type="password" id="password" label="Senha" disabled={this.props.mode === 'signup'} />
+                    </div>
+                    <div className="form-group form-group--signup">
+                        <Input type="text" id="fullname" label="Nome Completo" disabled={this.props.mode === 'login'} />
+                        <Input type="email" id="email" label="E-mail" disabled={this.props.mode === 'login'} />
+                        <Input type="password" id="createpassword" label="Senha" disabled={this.props.mode === 'login'} />
+                        <Input type="password" id="repeatpassword" label="Confirmar Senha" disabled={this.props.mode === 'login'} />
+                    </div>
                 </div>
-                <div className="form-group form-group--signup">
-                    <Input type="text" id="fullname" label="Nome Completo" disabled={this.props.mode === 'login'} />
-                    <Input type="email" id="email" label="E-mail" disabled={this.props.mode === 'login'} />
-                    <Input type="password" id="createpassword" label="Senha" disabled={this.props.mode === 'login'} />
-                    <Input type="password" id="repeatpassword" label="Confirmar Senha" disabled={this.props.mode === 'login'} />
-                </div>
-            </div>
-            <button className="button button--primary full-width" type="submit">{this.props.mode === 'login' ? 'Entrar' : 'Cadastrar'}</button>
-            <span> &#8594; Esqueci minha senha</span>
-        </form>
+                <button className="button button--primary full-width" type="submit">{this.props.mode === 'login' ? 'Entrar' : 'Cadastrar'}</button>
+                <span> &#8594; Esqueci minha senha</span>
+            </form>
         )
     }
 }
 
 const Input = ({ id, type, label, disabled }) => (
-    <input className="form-group__input" type={type} id={id} placeholder={label} disabled={disabled}/>
+    <input className="form-group__input" type={type} id={id} placeholder={label} disabled={disabled} />
 );
 
 const App = () => (
@@ -73,7 +79,7 @@ const App = () => (
         <LoginComponent
             mode={mode}
             onSubmit={
-                function() {
+                function () {
                     console.log('submit');
                 }
             }
@@ -81,5 +87,5 @@ const App = () => (
     </div>
 );
 
-ReactDOM.render( <App/>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
 
