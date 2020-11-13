579cf6593ca1cfd3c4a4919e88119de1000fcdb1
S4muk4SDF <sdf.dias.faria@gmail.com>
2020-11-13 19:46:44 -0300

    Pagina inicio modulo-2 renderizada

---
+++ izibug/src/components/modulo_2/sidebar-profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function sidebar_profile() {
+    return (
+        <div id="teste">
+            <h1>teste</h1>
+        </div>
+    )
+}
+
+export default sidebar_profile;
--- izibug/src/index.js
+++ izibug/src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Modulo_1 from './components/modulo_1'
 import ForgetPassword from './components/modulo_1/esqueci-senha'
 
+import sidebar_profile from './components/modulo_2/sidebar-profile'
+
 import './css/style.css';
 
 const App = () => (
@@ -12,7 +14,9 @@ const App = () => (
     <>
         <Route exact path="/" component={Modulo_1} />
 
-        <Route path="/esqueci-minha-senha" component={ForgetPassword} />
+        <Route exact path="/esqueci-minha-senha" component={ForgetPassword} />
+
+        <Route path="/modulo-2" component={sidebar_profile} />
     </>
 
 );
