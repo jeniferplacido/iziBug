ba5c19bba4e134914d067d0a68bb539bfbd3d46d
Dandara <dandara.dascanio@gmail.com>
2020-11-13 23:40:30 -0300

    construindo página esqueci minha senha

---
+++ izibug/src/components/modulo_1/esqueci-senha/esqueci-form/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import './style.css'
+
+class Esqueci_Form extends Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+
+  
+
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <div className="form-block__input-wrapper--forgetPass">
+          <div className="form-group--forgetPass form-group--signup--forgetPass ">
+            <input className="form-group__input--forgetPass" type="email" id="email" placeholder="Confirmar e-mail" disabled={this.props.mode === 'login'} />
+            
+          </div>
+        </div>
+        <button className="button--forgetPass button--primary--forgetPass full-width" type="submit">Enviar</button>
+      </form>
+    )
+  }
+}
+
+export default Esqueci_Form;
--- izibug/src/components/modulo_1/esqueci-senha/index.js
+++ izibug/src/components/modulo_1/esqueci-senha/index.js
@@ -1,6 +1,10 @@
 import { Component } from 'react'
 
+import Tela_Esqueci_Senha from './tela-esqueci-senha'
+
 import './style.css'
+
+import '../../../css/style.css'
 
 class ForgetPassword extends Component {
   // eslint-disable-next-line
@@ -14,10 +18,7 @@ class ForgetPassword extends Component {
 
   render() {
     return (
-      <div id="teste">
-        <h1>Teste</h1>
-        <button>Teste</button>
-      </div>
+      <Tela_Esqueci_Senha />
     )
   }
 }
