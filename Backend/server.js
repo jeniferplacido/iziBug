efe207f472701151e7c2f3393eb6e4d464991a57
Dani Rocha <danioliver_17@hotmail.com>
2020-11-19 21:53:28 -0300

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,24 @@ const connectDB = require('./functions/connectDB');
 const app = express();
 const allowCors = require('./middlewares/cors')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 process.env.PORT = config.server.porta;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
 // Rotas da API
-app.use(allowCors)
+app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use('/', require('./routes/index')); // Rotas da API
 app.use('/tasks', require('./routes/tasksRoutes'));
 app.use('/user', require('./routes/usersRoutes'));
