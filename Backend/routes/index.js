029d6d1263055839a0841935e075566f5cddfbf5
Dani Rocha <danioliver_17@hotmail.com>
2020-11-19 23:13:05 -0300

    .

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.send('API online na porta: '+ process.env.PORT);
+  res.send('API online na porta: ' + process.env.PORT);
 });
 
 router.post('/', (req, res) => {
-  res.send('API online na porta: '+ process.env.PORT);
+  res.send('API online na porta: ' + process.env.PORT);
 });
 
 module.exports = router;
