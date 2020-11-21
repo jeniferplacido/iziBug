a2e5840590734f401663032a5b3d69b0bbb477d9
S4muk4SDF <sdf.dias.faria@gmail.com>
2020-11-21 16:18:27 -0300

    Modal de confirmação para excluir da tela de visualizar usuarios admin concluido

--- izibug/src/components/modulo_3/tela-visualizar_usuarios-admin/section-users/index.js
+++ izibug/src/components/modulo_3/tela-visualizar_usuarios-admin/section-users/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 
 import './style.css'
 
 function Section_Users(props) {
-        return (
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [mensagemModal, setMensagemModal] = useState('')
+
+    function openModal() {
+        setModalIsOpen(true)
+        setMensagemModal('Tem certeza que desejar excluir este usuário?')
+    }
+
+    function closeModal() {
+        setModalIsOpen(false)
+    }
+
+    return (
+        <>
+            <Modal isOpen={modalIsOpen}>
+                <span onClick={closeModal}>X</span>
+                <h2>{mensagemModal}</h2>
+
+                <section>
+                    <h4 className="btn-option-yes">sim</h4>
+                    <h4 className="btn-option-no">não</h4>
+                </section>
+            </Modal>
+
             <div id="card-usuarios">
                 <h3>{props.userName}</h3>
                 <h3>{props.userEmail}</h3>
@@ -11,9 +36,10 @@ function Section_Users(props) {
                 <h3>{props.userEmpresa}</h3>
 
                 <img src="https://www.flaticon.com/svg/static/icons/svg/1160/1160515.svg"></img>
-                <span>X</span>
+                <span onClick={openModal}>X</span>
             </div>
-        )
+        </>
+    )
 }
 
 export default Section_Users
